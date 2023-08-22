7a1f915d21fb3a1112afff0c8889c0e91075919e
Arsh Sharma <arshsharma461@gmail.com>
2023-08-22 10:51:30 +0530

    initial commit
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oktetodo
+
+A todo list application showing how you can provision external resources (like an AWS S3 bucket in this case) your application might need with Okteto using Terraform.
---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.67.0"
+    }
+  }
+
+  backend "kubernetes" {
+    secret_suffix = "okteto"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 Bucket"
+  type        = string
+  default     = ""
+  validation {
+    condition     = length(var.bucket_name) > 1
+    error_message = "Please specify the name of the S3 bucket"
+  }
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "checks_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = true
+}
